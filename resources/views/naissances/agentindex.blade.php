1628645453303f0ba04f345e5a710a71fc3281fe
SalamJean <salamjeanlouis8@gmail.com>
2025-01-27 14:52:04 +0000

    template responsive6

--- resources/views/naissances/agentindex.blade.php
+++ resources/views/naissances/agentindex.blade.php
@@ -1,4 +1,4 @@
-td@extends('vendor.agent.layouts.template')
+@extends('vendor.agent.layouts.template')
 
 @section('content')
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
