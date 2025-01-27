3f10af5ae18c474d0f51897d4c3f7a8b79bd3f05
SalamJean <salamjeanlouis8@gmail.com>
2025-01-27 10:07:36 +0000

    error Target3

--- app/Models/CnpsAgent.php
+++ app/Models/CnpsAgent.php
@@ -5,6 +5,10 @@ namespace App\Models;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Notifications\Notifiable;
+use App\Models\NaissanceD;
+use App\Models\Naissance;
+use App\Models\Deces;
+use App\Models\Mariage;
 
 class CnpsAgent extends Authenticatable
 {
