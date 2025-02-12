45f0f1db1348a22a7d32e7c0ca3cb64e51029131
SalamJean <salamjeanlouis8@gmail.com>
2025-02-12 11:12:34 +0000

    responsive formulaire4

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -213,7 +213,7 @@
           <div class="text-danger" style="color: red; text-align:center">{{ $message }}</div>
           @enderror
       </div>
-    </div><br>
+    </div>
         <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
         <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
         <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
@@ -228,7 +228,7 @@
                 });
             });
         </script>
-        <button  type="submit">S'inscrire</button>
+        <button style="margin-top: 1px" type="submit">S'inscrire</button>
       </form>
     </div>
     <!-- Formulaire de connexion -->
