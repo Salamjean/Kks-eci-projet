9e3f9a6b7b06f0e0ee2683b9ad75e2b77a7224aa
SalamJean <salamjeanlouis8@gmail.com>
2025-03-01 21:28:25 +0000

    Infobip

--- resources/views/naissHop/details.blade.php
+++ resources/views/naissHop/details.blade.php
@@ -6,7 +6,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Liste des Patients</title>
+    <title>Liste des déclarations</title>
     
     <!-- Insertion de SweetAlert2 -->
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
